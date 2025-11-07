e37435b4d480dcfe38b10c948821fc7198c70085
Gor2112 <gooooooorgev@gmail.com>
2025-11-07 18:32:58 +0300

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,17 @@
+def is_palindrome(s, left, right):
+    if left >= right:
+        return True
+    if s[left] != s[right]:
+        return False
+    return is_palindrome(s, left + 1, right - 1)
+
+s = input("Введите строку: ")
+if is_palindrome(s, 0, len(s)-1):
+    print("Палиндром")
+else:
+    print("Не палиндром")
+
+# Пример ввода:
+# abba
+# Вывод:
+# Палиндром
